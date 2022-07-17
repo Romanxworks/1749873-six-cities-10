53706e382e3111bc2f87022590b92bf874d50774
Roman <chv-roman@mail.ru>
2022-07-17 17:54:28 +0300

    Маршрут по тайным тропам

--- project/src/pages/favorites-empty-page/favorites-empty-page.tsx
+++ project/src/pages/favorites-empty-page/favorites-empty-page.tsx
@@ -1,9 +1,10 @@
-import HeaderForMain from '../../components/header-component/header-main';
+import Header from '../../components/header/header';
+import Logo from '../../components/logo/logo';
 
 function FavoritesEmptyPage():JSX.Element{
   return(
     <div className="page page--favorites-empty">
-      <HeaderForMain />
+      <Header />
 
       <main className="page__main page__main--favorites page__main--favorites-empty">
         <div className="page__favorites-container container">
@@ -17,9 +18,7 @@ function FavoritesEmptyPage():JSX.Element{
         </div>
       </main>
       <footer className="footer">
-        <a className="footer__logo-link" href="main.html">
-          <img className="footer__logo" src="img/logo.svg" alt="6 cities logo" width="64" height="33"/>
-        </a>
+        <Logo />
       </footer>
     </div>
   );
--- project/src/pages/property-page/property-page.tsx
+++ project/src/pages/property-page/property-page.tsx
@@ -1,31 +1,31 @@
-import HeaderForMain from '../../components/header-component/header-main';
+import Header from '../../components/header/header';
 
 function PropertyPage():JSX.Element {
   return(
     <div className="page">
-      <HeaderForMain />
+      <Header />
 
       <main className="page__main page__main--property">
         <section className="property">
           <div className="property__gallery-container container">
             <div className="property__gallery">
               <div className="property__image-wrapper">
-                <img className="property__image" src="img/room.jpg" alt="Photo studio"/>
+                <img className="property__image" src="img/room.jpg" alt="studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
+                <img className="property__image" src="img/apartment-01.jpg" alt="studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-02.jpg" alt="Photo studio"/>
+                <img className="property__image" src="img/apartment-02.jpg" alt="studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-03.jpg" alt="Photo studio"/>
+                <img className="property__image" src="img/apartment-03.jpg" alt="studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="img/studio-01.jpg" alt="Photo studio"/>
+                <img className="property__image" src="img/studio-01.jpg" alt="studio"/>
               </div>
               <div className="property__image-wrapper">
-                <img className="property__image" src="img/apartment-01.jpg" alt="Photo studio"/>
+                <img className="property__image" src="img/apartment-01.jpg" alt="studio"/>
               </div>
             </div>
           </div>
@@ -207,8 +207,8 @@ function PropertyPage():JSX.Element {
             <div className="near-places__list places__list">
               <article className="near-places__card place-card">
                 <div className="near-places__image-wrapper place-card__image-wrapper">
-                  <a href="#">
-                    <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image"/>
+                  <a href="/">
+                    <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place"/>
                   </a>
                 </div>
                 <div className="place-card__info">
@@ -231,7 +231,7 @@ function PropertyPage():JSX.Element {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Wood and stone place</a>
+                    <a href="/">Wood and stone place</a>
                   </h2>
                   <p className="place-card__type">Private room</p>
                 </div>
@@ -239,8 +239,8 @@ function PropertyPage():JSX.Element {
 
               <article className="near-places__card place-card">
                 <div className="near-places__image-wrapper place-card__image-wrapper">
-                  <a href="#">
-                    <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place image"/>
+                  <a href="/">
+                    <img className="place-card__image" src="img/apartment-02.jpg" width="260" height="200" alt="Place"/>
                   </a>
                 </div>
                 <div className="place-card__info">
@@ -263,7 +263,7 @@ function PropertyPage():JSX.Element {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Canal View Prinsengracht</a>
+                    <a href="/">Canal View Prinsengracht</a>
                   </h2>
                   <p className="place-card__type">Apartment</p>
                 </div>
@@ -274,8 +274,8 @@ function PropertyPage():JSX.Element {
                   <span>Premium</span>
                 </div>
                 <div className="near-places__image-wrapper place-card__image-wrapper">
-                  <a href="#">
-                    <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place image"/>
+                  <a href="/">
+                    <img className="place-card__image" src="img/apartment-03.jpg" width="260" height="200" alt="Place"/>
                   </a>
                 </div>
                 <div className="place-card__info">
@@ -298,7 +298,7 @@ function PropertyPage():JSX.Element {
                     </div>
                   </div>
                   <h2 className="place-card__name">
-                    <a href="#">Nice, cozy, warm big bed apartment</a>
+                    <a href="/">Nice, cozy, warm big bed apartment</a>
                   </h2>
                   <p className="place-card__type">Apartment</p>
                 </div>
